dc10077a84899e5c3f1516ab153e61d58380903f
ouyangyu <ouyangyu_62@163.com>
2015-08-31 15:21:23 +0800

    course images

--- app/controllers/CourseController.php
+++ app/controllers/CourseController.php
@@ -3,9 +3,31 @@
 class CourseController extends ApiController {
 
     public function getResource($courseid , $moodleid = 1){
-        $str = "11-16-13";
+        $course = Course::where('courseid','=', $courseid)->where('moodleid','=', $moodleid)->first();
+        if(empty($course)) {
+            $this->errorjson('404','没有找到！');
+        } else {
 
-        $courseArray = explode('-',$str);
+            if(empty($course->courseimage)) {
+                $data['image'] = null;
+            } else {
+                $data['image'] = URL::to($course->courseimage);
+
+            }
+
+            $resources = Resource::where('courseid','=',$course->id)->get();
+
+            foreach($resources as $resource) {
+                $resource->resourceimage =  URL::to($resource->resourceimage);
+            }
+
+            $data['resource'] = $resources;
+            echo json_encode($data);
+        }
+    }
+
+    public function getImage($courseid , $moodleid = 1){
+        $courseArray = explode('-',$courseid);
         foreach($courseArray as $arr) {
             $course = null;
             $course = Course::where('courseid','=', $arr)->where('moodleid','=', $moodleid)->first();
@@ -23,26 +45,7 @@ class CourseController extends ApiController {
         }
 
         echo json_encode($result);
-        /*if(empty($course)) {
-            $this->errorjson('404','没有找到！');
-        } else {
 
-            if(!empty($course->courseimage)) {
-                $data['image'] = null;
-            } else {
-                $data['image'] = URL::to($course->courseimage);
-
-            }
-
-            $resources = Resource::where('courseid','=',$course->id)->get();
-
-            foreach($resources as $resource) {
-                $resource->resourceimage =  URL::to($resource->resourceimage);
-            }
-
-            $data['resource'] = $resources;
-            echo json_encode($data);
-        }*/
     }
 
 }
